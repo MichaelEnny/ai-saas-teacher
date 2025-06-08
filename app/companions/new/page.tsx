9b805d6685479e46806a576b91adc1548a408f9e
MichaelEnny <moe0100@mcdaniel.edu>
2025-06-07 18:49:28 -0700

    Implemented Auth, Billing, Database Setup, Create Companion Server action

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+//import CompanionForm from "@/components/CompanionForm";
+import {auth} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
+//import {newCompanionPermissions} from "@/lib/actions/companion.actions";
+import Image from "next/image";
+import Link from "next/link";
+import CompanionForm from "@/components/CompanionForm";
 
-const  NewCompanion = () => {
+const NewCompanion = async () => {
+    const { userId } = await auth();
+    if(!userId) redirect('/sign-in');
+
+    //const canCreateCompanion = await newCompanionPermissions();
+
     return (
-        <div>
-            NewCompanion
-        </div>
-    );
-};
+        <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+                <article className="w-full gap-4 flex flex-col">
+                    <h1>Companion Builder</h1>
 
-export default NewCompanion;
+                    <CompanionForm />
+                </article>
+        </main>
+    )
+}
+
+export default NewCompanion
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -9,7 +10,7 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "HybridIntel",
+  title: "EduVoxa",
   description: "Real-time AI Teaching Platform",
 };
 
@@ -19,11 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{variables: {colorPrimary: '#fe5933'}} }>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+        </body>
+      </html>
   );
 }
